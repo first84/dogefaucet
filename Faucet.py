e1988068a6b4a2f6d197358ce13fcdbd9b3c0897
uvok <gh@uvokchee.de>
2015-01-17 17:54:41 +0100

    Add logging & documentation

--- Faucet.py
+++ Faucet.py
@@ -1,54 +1,61 @@
 """ This is going to be a dogecoin faucet """
 
-
 # Database support
 import sqlite3
 # date / time calculations
 import datetime
 # URL fetching
 import requests
+# logging!
+import logging
 
 # wallet processings
 from block_io import BlockIo
 
-__version__ = "0.1"
+logging.basicConfig(level=logging.INFO)
 
+__version__ = "0.1"
 
 class Faucet:
     """ Faucet class
     
     Contains all methods neccessary to execute a faucet """
 
+    # database file name
     _filename = "doge.db"
     def __init__(self):
         pass
         
-    def add_payout_request(self,  address):
-        """ add an address to the payout """
+    # todo: Lock, to avoid double-adding from multiple conections ?
+    def add_payout_request(self,  address, force=False):
+        """ add an address to the payout 
+        
+        Returns True if payout was added, otherwise false """
         # waiting: datetime, address
         pass
         
     def check_payout_request(self, address):
-        """ Check if address is eligitable for a payout request"""
+        """ Check if address is eligible for a payout request
         
-        # waiting: datetime, address
-        pass
+        Returns True if payout request is okay, otherwise False"""
         
     def process_payouts(self, address):
-        """ process the payout requests """
+        """ (TODO) process the payout requests """
         
         # processed: datetime, payout, number_addresses, txid
         # processed_addresses: address, process_id (FK)
         pass
         
+    # TODO
     def get_balance(self):
-        """ Get faucet balance """
+        """ (TODO) Get faucet balance """
         # balance: datetime, amount
         pass
-        
+
+    #TODO
     @staticmethod
     def check_address_validity(address):
-        """ check if address is valid """
+        """ (TODO) check if address is valid """
         pass
 
 if __name__ == '__main__':
