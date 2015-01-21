2407b664d180ed596b719adb4841b1e87cb7a4d5
uvok <gh@uvokchee.de>
2015-01-21 20:48:59 +0100

    Implement get_balance on BlockIoPaymentProcessor

--- Faucet/BlockIoPaymentProcessor.py
+++ Faucet/BlockIoPaymentProcessor.py
@@ -1,17 +1,25 @@
 """ Contains a block.io payment processor """
 
+import logging
+import logging.config
+
 from block_io import BlockIo
 import blockioconfig
 apiversion = 2 # API version
 
 from AbstractPaymentProcessor import AbstractPaymentProcessor
 
+import abc
+
 class BlockIoPaymentProcessor(AbstractPaymentProcessor):
     """ Class for executing faucet payments on block.io """
     
-    _block_io = BlockIo(blockioconfig.apikey, blockioconfig.secretpin, 
-                        apiversion)
-    
+    def __init__(self):
+        self._logger = logging.getLogger(__name__)
+        self._logger.info("Creating BlockIoPaymentProcessor instance")
+        self._block_io = BlockIo(blockioconfig.apikey, blockioconfig.secretpin,
+                                    apiversion)
+
     def execute_payment(self, destination_address, amount):
         """ Execute a payment to one receiving single address
         
@@ -23,7 +31,7 @@ class BlockIoPaymentProcessor(AbstractPaymentProcessor):
         
         return the transaction id or None """
         raise NotImplementedError("Not implemented yet")
-        
+
     def get_transaction_status(self):
         """ Get the status of the transaction
         
@@ -32,5 +40,29 @@ class BlockIoPaymentProcessor(AbstractPaymentProcessor):
          - False if unconfirmed
          - None if transaction doesn't exist """
         raise NotImplementedError("Not implemented yet")
+
+    def get_available_balance(self, address=None):
+        """ Get the available balance
+
+        Keyword arguments:
+
+        address -- Adress to get balance from. Valid address or None for total
+                    balance"""
+        reply=None
+        if address:
+            reply = self._block_io.get_address_balance(addresses=address)
+        else:
+            reply = self._block_io.get_balance()
+
+        if reply["status"] == "success":
+            # can this cause problems?
+            return float(reply["data"]["available_balance"])
+        else:
+            # ???, maybe raise an exeption?
+            raise Exception("Error querying block.io: %s", reply)
+
     
-    
+if __name__ == "__main__":
+    b=BlockIoPaymentProcessor()
+    rep = b.get_available_balance()
+    print "%f" % (rep, )
